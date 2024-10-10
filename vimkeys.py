8f38738b70213c75e8eeca12204da33961d24f3c
ryan <exogenesis@protonmail.com>
2024-10-10 22:16:51 +1100

    Refactor Highlighting Rules into Editor Module

--- vimkeys.py
+++ vimkeys.py
@@ -1,11 +1,20 @@
 from PyQt6.QtWidgets import QTextEdit, QFrame, QVBoxLayout
 from markdown_utils import WebEngineViewWithBaseUrl
-import os
-from PyQt6.QtCore import QUrl, Qt
-from PyQt6.QtGui import QKeyEvent, QTextCursor, QColor, QTextFormat
-from PyQt6.QtWebEngineWidgets import QWebEngineView
+from PyQt6.QtCore import Qt
 import re
 from markdown_utils import Markdown
+
+
+from PyQt6.QtCore import Qt, QRegularExpression
+from PyQt6.QtGui import (
+    QSyntaxHighlighter,
+    QTextCharFormat,
+    QFont,
+    QColor,
+    QKeyEvent,
+    QTextCursor,
+    QTextFormat,
+)
 
 
 class WebPopupInTextEdit:
@@ -302,3 +311,77 @@ class VimTextEdit(QTextEdit):
     def set_dark_mode(self, is_dark):
         self.dark_mode = is_dark
         self.web_popup.set_dark_mode(is_dark)
+
+
+class MarkdownHighlighter(QSyntaxHighlighter):
+    """Syntax highlighter for Markdown code in QTextEdit."""
+
+    def __init__(self, document):
+        super().__init__(document)
+        # Define the highlighting rules
+        self.highlightingRules = []
+
+        # Heading format
+        for i in range(1, 7):
+            headingFormat = QTextCharFormat()
+            headingFormat.setFontWeight(QFont.Weight.Bold)
+            headingFormat.setForeground(QColor("blue"))
+            headingFormat.setFontPointSize(24 - i * 2)
+            hashes = "#" * i
+            self.highlightingRules.append(
+                (QRegularExpression(f"^{hashes} .+"), headingFormat)
+            )
+
+        # Bold format
+        boldFormat = QTextCharFormat()
+        boldFormat.setFontWeight(QFont.Weight.Bold)
+        self.highlightingRules.append(
+            (QRegularExpression("\\*\\*(.*?)\\*\\*"), boldFormat)
+        )
+        self.highlightingRules.append((QRegularExpression("__(.*?)__"), boldFormat))
+
+        # Italic format
+        italicFormat = QTextCharFormat()
+        italicFormat.setFontItalic(True)
+        self.highlightingRules.append((QRegularExpression("\\*(.*?)\\*"), italicFormat))
+        self.highlightingRules.append((QRegularExpression("_(.*?)_"), italicFormat))
+
+        # Code format
+        codeFormat = QTextCharFormat()
+        codeFormat.setFontFamily("Courier")
+        codeFormat.setForeground(QColor("darkGreen"))
+        self.highlightingRules.append((QRegularExpression("`[^`]+`"), codeFormat))
+        self.highlightingRules.append((QRegularExpression("^\\s*```.*"), codeFormat))
+
+        # Link format
+        linkFormat = QTextCharFormat()
+        linkFormat.setForeground(QColor("darkBlue"))
+        self.highlightingRules.append(
+            (QRegularExpression("\\[.*?\\]\\(.*?\\)"), linkFormat)
+        )
+
+        # Image format
+        imageFormat = QTextCharFormat()
+        imageFormat.setForeground(QColor("darkMagenta"))
+        self.highlightingRules.append(
+            (QRegularExpression("!\\[.*?\\]\\(.*?\\)"), imageFormat)
+        )
+
+        # List format
+        listFormat = QTextCharFormat()
+        listFormat.setForeground(QColor("brown"))
+        self.highlightingRules.append(
+            (QRegularExpression("^\\s*([-+*])\\s+.*"), listFormat)
+        )
+        self.highlightingRules.append(
+            (QRegularExpression("^\\s*\\d+\\.\\s+.*"), listFormat)
+        )
+
+    def highlightBlock(self, text):
+        for pattern, format in self.highlightingRules:
+            iterator = pattern.globalMatch(text)
+            while iterator.hasNext():
+                match = iterator.next()
+                index = match.capturedStart()
+                length = match.capturedLength()
+                self.setFormat(index, length, format)
